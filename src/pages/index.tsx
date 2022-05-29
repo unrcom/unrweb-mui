c77badde71fe0e7d4ab3d64ec9f404b594865217
ishi32 <ishi32@unremoted.com>
2022-05-29 13:01:42 +0900

    Code 3rd commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+
 import { Layout } from "../components/Layout";
 
 export default function Home() {
   return (
     <Layout title="Home">
-      <Typography className="text-4xl">Welcome to unremoted.com</Typography>
+      <Grid container spacing={0} className="margin-top60">
+        <Grid item xs={12} alignItems="center" className="padding20">
+          <Typography variant="h3">Welcome to unremoted.com</Typography>
+        </Grid>
+      </Grid>
     </Layout>
   );
 }
